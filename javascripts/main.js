8cc5da17d6835f03cb590c223c8aee951c9a5f73
sharon smith <sharon.e.smith2@gmail.com>
2017-04-20 21:47:04 -0500

    variable path names for treehouse URL's + event handler for button

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,38 +1,80 @@
 
 $(document).ready(function(){
 
+// geoffwebb
+// ellisthomas
+
 let person1 = [];
 let person2 = [];
 
-const loadTreeHse1 = () => {
+
+// event handler for <Start Cage Match>
+$("#go").on("click", function(){
+// console.log("clicked");
+	Promise.all([loadTreeHse1(), loadTreeHse2()])
+	.then(function(result){
+// console.log("result :: ", result);
+		result.forEach(function(treehouseData, index){
+			if (index === 0) {
+				person1.push(treehouseData);
+			} else {
+				person2.push(treehouseData);
+			}
+console.log("person1 :: ", person1);
+console.log("person2 :: ", person2);
+		})
+		.catch(function(fileError){
+			alert("This person does not have a Treehouse account ...");
+		});
+	});
+});
+
+
+// event handerl will read text input string, e.g., "geoffwebb"
+// receives back from buildURL the ajax url string
+
+const buildURL = (userInputString) => {
+console.log("user text input :: ", $("#userInput1").val());
+console.log("user text input :: ", $("#userInput2").val());
+console.log("printing URL :: ", "https://teamtreehouse.com/" + userInputString + ".json");
+	return "https://teamtreehouse.com/" + userInputString + ".json";
+};
+
+
+const loadTreeHse1 = (person1URL) => {
 	return new Promise ((resolve, reject) => {
-		$.ajax("https://teamtreehouse.com/geoffwebb.json")
+		$.ajax(buildURL($("#userInput1").val()))
+		// $.ajax("https://teamtreehouse.com/geoffwebb.json")
 		.done ((data1) => resolve(data1))
 		.fail ((error) => reject(error));
 	});
 };
 
-const loadTreeHse2 = () => {
+const loadTreeHse2 = (person2URL) => {
 	return new Promise ((resolve, reject) => {
-		$.ajax("https://teamtreehouse.com/ellisthomas.json")
+		$.ajax(buildURL($("#userInput2").val()))
+		// $.ajax("https://teamtreehouse.com/ellisthomas.json")
 		.done ((data2) => resolve(data2))
 		.fail ((error) => reject(error));
 	});
 };
 
-Promise.all([loadTreeHse1(), loadTreeHse2()])
-.then(function(result){
+// Promise.all([loadTreeHse1(), loadTreeHse2()])
+// .then(function(result){
 // console.log("result :: ", result);
-	result.forEach(function(treehouseData, index){
-		if (index === 0) {
-			person1.push(treehouseData);
-		} else {
-			person2.push(treehouseData);
-		}
-console.log("person1 :: ", person1);
-console.log("person2 :: ", person2);
-	});
-});
+// 	result.forEach(function(treehouseData, index){
+// 		if (index === 0) {
+// 			person1.push(treehouseData);
+// 		} else {
+// 			person2.push(treehouseData);
+// 		}
+// console.log("person1 :: ", person1);
+// console.log("person2 :: ", person2);
+// 	})
+// 	.catch(function(fileError){
+// 		alert("This person does not have a Treehouse account ...");
+// 	});
+// });
 
 
 
