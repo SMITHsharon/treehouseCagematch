6c23c966dd8f4c7d35712d3e098a3de9e4580b13
sharon smith <sharon.e.smith2@gmail.com>
2017-04-24 18:01:54 -0500

    final working code / some questions

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,6 @@
 
 $(document).ready(function(){
 
-// geoffwebb
-// ellisthomas
 
 let person1 = [];
 let person2 = [];
@@ -10,7 +8,7 @@ let person2 = [];
 
 // function writes the output to the DOM
 // after parsing the Treehouse JSON data:
-// * displays the profile picture for each of the two profiles
+// * displays the profile picture for each of the specified profiles
 // * displays the total number of points for each profile
 // * clearly displays the cage match winner (person with most points)
 // * displays a button, <Show Badges>
@@ -56,43 +54,45 @@ const writeOutput = () => {
 };
 
 
-// function returns the total points earned by the given Player
-const getPoints = (thisGuy) => {	
-	let pointsArray = thisGuy.points;
-	return pointsArray.total;
-};
+// function returns the Total Points earned by the given Player
+	const getPoints = (thisGuy) => {	
+		let pointsArray = thisGuy.points;
+		return pointsArray.total;
+	};
 
 
 // function determines the Winner of the Cage Match
 // i.e., Player with the most points
-const getWinnerName = (person1, person2) => {
+// returns that person's Profile Name
+	const getWinnerName = (person1, person2) => {
 
-	let pointsArray1 = person1.points;
-	let pointsArray2 = person2.points;
+		let pointsArray1 = person1.points;
+		let pointsArray2 = person2.points;
 
-	if (pointsArray1.total > pointsArray2.total) {
-		return person1.profile_name;
-	} else {
-		return person2.profile_name;
-	}
-};
+		if (pointsArray1.total > pointsArray2.total) {
+			return person1.profile_name;
+		} else {
+			return person2.profile_name;
+		}
+	};
 
 
 // function returns an array of the Winner's Treehouse badges
-const getWinnerBadges = () => {
+	const getWinnerBadges = () => {
 
-	let pointsArray1 = person1.points;
-	let pointsArray2 = person2.points;
+		let pointsArray1 = person1.points;
+		let pointsArray2 = person2.points;
 
-	if (pointsArray1.total > pointsArray2.total) {
-		return person1.badges;
-	} else {
-		return person2.badges;
-	}
-};
+		if (pointsArray1.total > pointsArray2.total) {
+			return person1.badges;
+		} else {
+			return person2.badges;
+		}
+	};
 
 
-// function writes the Winner's Treehouse badges to the DOM
+// function writes the Winner's Treehouse Badges to the DOM
+// laid out in Bootstraps row/column grid structure, six badges per row
 const showWinnerBadges = (winnerBadges) => {
 
 	let winner = getWinnerName(person1, person2).toUpperCase();
@@ -113,6 +113,8 @@ const showWinnerBadges = (winnerBadges) => {
 	var colCounter = 0;
 	for (let i=0; i<winnerBadges.length; i++) {
 		domString += `<div class="col-sm-2">`;
+
+		// assign different class names to the badges, for use in the animation event handlers
 		if (i%6 === 0) {
 			domString += `<img class="badgeURL thumbnail badgeImage1" height="190" width="190" src="${winnerBadges[i].icon_url}" alt="course badge">`;
 		} else if (i%5 === 1) {
@@ -142,137 +144,123 @@ const showWinnerBadges = (winnerBadges) => {
 };
 
 
-$("body").on("click", ".winnerName", () => {
-	$(".winnerName").animate({
-    	fontSize: "40px",
-	}, 1500 );
-});
-
-
-$("body").on("click", ".winnerName", () => {
-	$(".wins").animate({
-    	fontSize: "40px",
-	}, 1500 );
-});
-
-
-$("body").on("click", "#badgeNameHeader", () => {
-  $("#badgeNameHeader").animate({
-    fontSize: "25px",
-  }, 1500 );
-});
-
-
-// $("body").on("click", ".badgeImage2", () => {
-//   $( ".badgeImage2" ).animate({
-//     opacity: 0.25,
-//     left: "+=50",
-//     height: "toggle"
-//   }, 5000, () => {
-//     // Animation complete.
-//   });
-// });
-
-
-$("body").on("click", ".badgeImage3", () => {
-  $( ".badgeImage2" ).animate({
-    opacity: 0.10,
-    right: "+=75",
-    height: "toggle"
-  }, 2500, () => {
-    // Animation complete.
-  });
-});
-
-
-$("body").on("click", ".badgeImage5", () => {
-  $( ".badgeImage4" ).animate({
-    opacity: 0.25,
-    right: "+=50",
-    width: "toggle"
-  }, 5000, () => {
-    // Animation complete.
-  });
-});
-
-
-$("body").on("click", ".badgeImage1", () => {
-  $( ".badgeImage6" ).animate({
-    opacity: 0.40,
-    right: "+=15",
-    height: "toggle"
-  }, 5000, () => {
-    // Animation complete.
-  });
-});
-
-
-$("body").on("click", ".badgeURL", () => {
-  $( ".badgeURL" ).animate({
-    borderBottomWidth: "6px",
-    borderRightWidth: "3px"
-  }, 1500 );
-});
-
-
+//****************************************************
 // event handler for <Show Badges> button
 // calls function that displays the Winner's badges
-// $("body").on("click", "#showBadges", () => {
-// 	jQuery("#block").animate({
-// 			backgroundColor: jQuery.Color({ saturation: 0 })
-// 	}, 1500 );
-// })
-// .showWinnerBadges(getWinnerBadges()
-// );
-
-$("body").on("click", "#showBadges", () => {
-	showWinnerBadges(getWinnerBadges());
-});
+//****************************************************
+	$("body").on("click", "#showBadges", () => {
+		showWinnerBadges(getWinnerBadges());
+	});
 
 
-// .rotate() DEMO
-// $(function() {
-$("body").on("click", ".badgeImage1", () => {
-    $(this).rotate();
-});
+//****************************************************
+// ANIMATIONS CODE
+//****************************************************
+	$("body").on("click", ".winnerName", () => {
+		$(".winnerName").animate({
+	    	fontSize: "40px",
+		}, 1500 );
+	});
+	// how to chain these two functions?
+	$("body").on("click", ".winnerName", () => {
+		$(".wins").animate({
+	    	fontSize: "40px",
+		}, 1500 );
+	});
 
 
-$("body").on("click", ".badgeImage2", () => {
-    $(this).rotate({ count:4, duration:0.6, easing:'ease-out' });
-});
+	$("body").on("click", "#badgeNameHeader", () => {
+	  $("#badgeNameHeader").animate({
+	    fontSize: "25px",
+	  }, 1500 );
+	});
 
 
-$("body").on("click", ".badgeImage3", () => {
-    $(this).rotate({ endDeg:180, persist:true });
-	  }, function() {
-	    $(this).rotate({ endDeg:360 });
-});
+	$("body").on("click", ".badgeImage3", () => {
+	  $( ".badgeImage2" ).animate({
+	    opacity: 0.10,
+	    right: "+=75",
+	    height: "toggle"
+	  }, 2500, () => {
+	    // Animation complete.
+	  });
+	});
 
 
-$("body").on("click", ".badgeImage4", () => {
-    $(this).rotate({ endDeg:180, persist:true });
-	  }, function() {
-	    $(this).rotate({ endDeg:-360, duration:0.8, easing:'ease-in' });
-});
+	$("body").on("click", ".badgeImage5", () => {
+	  $( ".badgeImage4" ).animate({
+	    opacity: 0.25,
+	    right: "+=50",
+	    width: "toggle"
+	  }, 5000, () => {
+	    // Animation complete.
+	  });
+	});
 
 
-$("body").on("click", ".badgeImage6", () => {
-    $(this)
-      .css({ position:'relative', left:0 })
-      .rotate({ count:2, easing:'ease-in', animate:{ left:120 } })
-      .fadeTo(400, 0.1)
-      .fadeTo(300, 1)
-      .delay(200)
-      .rotate({ endDeg:-360, count:3, easing:'ease-out', animate:{ left:0 } });
-});
+	$("body").on("click", ".badgeImage1", () => {
+	  $( ".badgeImage6" ).animate({
+	    opacity: 0.40,
+	    right: "+=15",
+	    height: "toggle"
+	  }, 5000, () => {
+	    // Animation complete.
+	  });
+	});
 
 
-$("badgeImage5").toggle(function() {
-    $(this).rotate({ count:99999, forceJS:true });
-  }, function() {
-    $(this).stop();
-  });
+	$("body").on("click", ".badgeURL", () => {
+	  $( ".badgeURL" ).animate({
+	    borderBottomWidth: "6px",
+	    borderRightWidth: "3px"
+	  }, 1500 );
+	});
 
+
+// trying to rotate badges
+// but not working ???
+	// .rotate() DEMO
+	// $(function() {
+	$("body").on("click", ".badgeImage1", () => {
+	    $(this).rotate();
+	});
+
+
+	$("body").on("click", ".badgeImage2", () => {
+	    $(this).rotate({ count:4, duration:0.6, easing:'ease-out' });
+	});
+
+
+	$("body").on("click", ".badgeImage3", () => {
+	    $(this).rotate({ endDeg:180, persist:true });
+		  },  ()  => {
+		    $(this).rotate({ endDeg:360 });
+	});
+
+
+	$("body").on("click", ".badgeImage4", () => {
+	    $(this).rotate({ endDeg:180, persist:true });
+		  },  ()  => {
+		    $(this).rotate({ endDeg:-360, duration:0.8, easing:'ease-in' });
+	});
+
+
+	$("body").on("click", ".badgeImage6", () => {
+	    $(this)
+	      .css({ position:'relative', left:0 })
+	      .rotate({ count:2, easing:'ease-in', animate:{ left:120 } })
+	      .fadeTo(400, 0.1)
+	      .fadeTo(300, 1)
+	      .delay(200)
+	      .rotate({ endDeg:-360, count:3, easing:'ease-out', animate:{ left:0 } });
+	});
+
+
+	$("badgeImage5").toggle(function() {
+	    $(this).rotate({ count:99999, forceJS:true });
+	  },  ()  => {
+	    $(this).stop();
+	  });
 
 
 // const rotation = () => {
@@ -289,50 +277,55 @@ $("badgeImage5").toggle(function() {
 
 
 
+//****************************************************
 // event handler will read text input string, e.g., "geoffwebb"
 // receives back from buildURL the ajax url string
-const buildURL = (userInputString) => {
-	return "https://teamtreehouse.com/" + userInputString + ".json";
-};
+//****************************************************
+	const buildURL = (userInputString) => {
+		return "https://teamtreehouse.com/" + userInputString + ".json";
+	};
 
-
-const loadTreeHse1 = (person1URL) => {
-	return new Promise ((resolve, reject) => {
-		// $.ajax(buildURL($("#userInput1").val()))
-		$.ajax("https://teamtreehouse.com/geoffwebb.json")
-		.done ((data1) => resolve(data1))
-		.fail ((error) => reject(error));
-	});
-};
-
-const loadTreeHse2 = (person2URL) => {
-	return new Promise ((resolve, reject) => {
-		// $.ajax(buildURL($("#userInput2").val()))
-		$.ajax("https://teamtreehouse.com/ellisthomas.json")
-		.done ((data2) => resolve(data2))
-		.fail ((error) => reject(error));
-	});
-};
-
-
-$("#go").on("click", () => {
-
-	Promise.all([loadTreeHse1(), loadTreeHse2()])
-	.then(function(result){
-		result.forEach( (treehouseData, index) => {
-			if (index === 0) {
-				person1 = treehouseData;
-			} else {
-				person2 = treehouseData;
-			}
+//****************************************************
+// Promises code to get and load the Treehouse <json> files
+//****************************************************
+	const loadTreeHse1 = (person1URL) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(buildURL($("#userInput1").val()))
+			// $.ajax("https://teamtreehouse.com/geoffwebb.json")
+			.done ((data1) => resolve(data1))
+			.fail ((error) => reject(error));
 		});
+	};
 
-		writeOutput();
-	})
-	.catch( (fileError) => {
-		alert("This person does not have a Treehouse account ...");
+	const loadTreeHse2 = (person2URL) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(buildURL($("#userInput2").val()))
+			// $.ajax("https://teamtreehouse.com/ellisthomas.json")
+			.done ((data2) => resolve(data2))
+			.fail ((error) => reject(error));
+		});
+	};
+
+
+	$("#go").on("click", () => {
+
+		Promise.all([loadTreeHse1(), loadTreeHse2()])
+		.then( (result) => 
+		{
+			result.forEach( (treehouseData, index) => {
+				if (index === 0) {
+					person1 = treehouseData;
+				} else {
+					person2 = treehouseData;
+				}
+			});
+
+			writeOutput();
+		})
+		.catch( (fileError) => {
+			alert("This person does not have a Treehouse account ...");
+		});
 	});
-});
 
 
 // Promise.all([loadTreeHse1(), loadTreeHse2()])
@@ -349,6 +342,5 @@ $("#go").on("click", () => {
 // 		alert("This person does not have a Treehouse account ...");
 // 	});
 // });
-
 
 });
